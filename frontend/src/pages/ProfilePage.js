64c2eea4786cc5ec2a3f6bc8a575bfcb70a41262
Hasib <hasibhaque@gmail.com>
2024-10-21 14:36:08 +0600

    worked on profile page

---
+++ frontend/src/pages/ProfilePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ProfilePage = () => {
+
+  const location = useLocation();
+
+  const user = location.state.user;
+  const profilePhotoUrl = location.state.profilePhotoUrl;
+
+  console.log(location.state);
+  return (
+    <div>
+      <h1>profile page</h1>
+      <img src={profilePhotoUrl} alt='profile' style={{ width: '150px', height: '150px' }}/>
+      <p><strong>Name: </strong>{user.name}</p>
+      <p><strong>Username: </strong>{user.username}</p>
+      <p><strong>Email: </strong>{user.email}</p>
+      {/* <p><strong>Password: </strong>{user.password}</p> */}
+    </div>
+  )
+}
+
+export default ProfilePage
